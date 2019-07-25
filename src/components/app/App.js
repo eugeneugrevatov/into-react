10abed5928410517126f5f01d111956c56bc743c
Eugene Ugrevatov <e.ugrevatov@gmail.com>
2019-07-25 15:11:15 +0300

    proper routing

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import User from '../user/User';
+import UserEdit from '../user-edit/UserEdit';
 import Header from '../header/Header';
 import UserList from '../user-list/UserList';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import styles from './App.module.css'
+import UserDetails from "../user-details/UserDetails";
 
 function App() {
 
@@ -13,13 +14,15 @@ function App() {
             <div className={styles.app}>
                 <Header/>
                 <main>
-                    <Route exact path={['/', '/users']} component={UserList}/>
-                    <Route exact path='/user/:id(\d+)' component={User}/>
-                    <Route exact path='/user/new' component={User}/>
+                    <Switch>
+                        <Route exact path={['/', '/users']} component={UserList}/>
+                        <Route exact path='/user/new' component={UserEdit}/>
+                        <Route exact path='/user/:id/details' component={UserDetails}/>
+                        <Route exact path='/user/:id' component={UserEdit}/>
+                        <Route exact render={() => <h1 style={{textAlign: 'center'}}>4 0 4</h1>}/>
+                    </Switch>
                 </main>
             </div>
-
-
         </Router>
     );
 }
