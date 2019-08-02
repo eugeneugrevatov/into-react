8c4fe59a84c3b970f35748cfdffe8bdfc08887e3
Eugene Ugrevatov <e.ugrevatov@gmail.com>
2019-08-02 16:20:43 +0300

    private routes

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,25 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import UserEdit from '../user-edit/UserEdit';
 import Header from '../header/Header';
 import UserList from '../user-list/UserList';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import styles from './App.module.css'
 import UserDetails from "../user-details/UserDetails";
+import Login from "../login/Login";
+import AuthService from "../../services/auth";
+import {configureFakeFetch} from "../../be-mock/fake-fetch";
+import PrivateRoute from "../private-route/PrivateRoute";
+
+configureFakeFetch();
 
 function App() {
 
+    const [user, setUser] = useState(AuthService.getCurrentUser());
 
     return (
         <Router>
             <div className={styles.app}>
-                <Header/>
+                <Header user={user} setUser={setUser}/>
                 <main>
                     <Switch>
-                        <Route exact path={['/', '/users']} component={UserList}/>
-                        <Route exact path='/user/new' component={UserEdit}/>
-                        <Route exact path='/user/:id/details' component={UserDetails}/>
-                        <Route exact path='/user/:id' component={UserEdit}/>
-                        <Route exact render={() => <h1 style={{textAlign: 'center'}}>4 0 4</h1>}/>
+                        <Route exact path={'/login'}
+                               render={routeProps => <Login {...routeProps} user={user} setUser={setUser}/>}/>
+
+                        <PrivateRoute exact path={['/', '/users']} component={UserList}/>
+                        <PrivateRoute exact path='/user/new' component={UserEdit}/>
+                        <PrivateRoute exact path='/user/:id/details' component={UserDetails}/>
+                        <PrivateRoute exact path='/user/:id' component={UserEdit}/>
+                        <PrivateRoute exact render={() => <h1 style={{textAlign: 'center'}}>4 0 4</h1>}/>
                     </Switch>
                 </main>
             </div>
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import styles from './Header.module.css'
+import AuthService from '../../services/auth'
 import {Link} from "react-router-dom";
 
-function Header() {
+function Header({user, setUser}) {
+
+    const logout = () => {
+        AuthService.logout();
+        setUser(null);
+    };
+
     return (
         <header className={styles.header}>
-            <h1>My simple app</h1>
-            <nav>
-                <Link to="/users">Users</Link>
-            </nav>
+            <div className={styles.left}>
+                <span>My simple app</span>
+            </div>
+            {user &&
+            <div className={styles.right}>
+                <span>{`${user.firstName} ${user.lastName}`}</span>
+                <button onClick={logout}>
+                    <Link to={'/login'} on>Log out</Link>
+                </button>
+            </div>
+            }
         </header>
     );
 }
