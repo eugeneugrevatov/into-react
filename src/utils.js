a6e428c1677cfa79f1d028189ad582e6b0858b8a
Eugene Ugrevatov <e.ugrevatov@gmail.com>
2019-08-02 16:15:04 +0300

    ignore package-lock

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,10 @@
-export const print = obj => console.log(JSON.stringify(obj, null, 2));
+export const toObj = str => JSON.parse(str);
+
+export const toStr = obj => JSON.stringify(obj);
+export const toStrP = obj => JSON.stringify(obj, null, 2);
+
+export const logObj = obj => console.log(toStr(obj));
+export const logObjP = obj => console.log(toStrP(obj));
+
+export const alertObj = obj => alert(toStr(obj));
+export const alertObjP = obj => alert(toStrP(obj));
